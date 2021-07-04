a96b8f4ecb41b85fcf84e886a1e5b9431c85bc6c
biechee <andy50902@gmail.com>
2021-07-04 20:30:50 +0800

    3

---
+++ app/src/main/java/com/example/greatsleep/ClockAlarm.java
@@ -0,0 +1,82 @@
+package com.example.greatsleep;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.WindowManager;
+import android.media.MediaPlayer;
+
+public class ClockAlarm extends AppCompatActivity {
+    SharedPreferences preferences;
+    private MediaPlayer mediaPlayer;
+    private Vibrator v;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_alarm);
+
+        v = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+
+        //取得鈴聲設定
+        preferences = getSharedPreferences("alarm_tune", Context.MODE_PRIVATE);
+        mediaPlayer=MediaPlayer.create(this,preferences.getInt("tune",0));
+
+        //突破鎖屏代碼
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock( "" );
+        keyguardLock.disableKeyguard();
+        //彈出對話框
+        AlertDialog.Builder ab = new AlertDialog.Builder(ClockAlarm.this);
+        ab.setTitle("起床了!!");
+        ab.setMessage("時間到囉~~~")
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //你可以在這裡加入事件
+                        v.cancel();
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                        mediaPlayer=null;
+                        ClockAlarm.this.finish();
+                    }
+                });
+
+        //點選對話框外動作
+        ab.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                v.cancel();
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer=null;
+                ClockAlarm.this.finish();
+            }
+        });
+        AlertDialog ad = ab.create();
+        ad.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //振動器
+        Boolean isVibrate=preferences.getBoolean("vibration",true);
+        if(isVibrate){
+            v.vibrate(new long[]{1000, 1000, 1000, 1000}, 0);
+        }
+        else{
+            v.cancel();
+        }
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+    }
+}
---
+++ app/src/main/java/com/example/greatsleep/ClockSound.java
@@ -0,0 +1,68 @@
+package com.example.greatsleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.File;
+
+public class ClockSound extends AppCompatActivity {
+    private RadioGroup mRadioGroup;
+    private RadioButton mRadioButton0,mRadioButton1,mRadioButton2;
+    AlarmSet sound = new AlarmSet(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_sound);
+
+        SharedPreferences preferences = getSharedPreferences("alarm_tune", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+
+        mRadioButton0 = (RadioButton) findViewById(R.id.mRadioButton0);
+        mRadioButton1 = (RadioButton) findViewById(R.id.mRadioButton1);
+        mRadioButton2 = (RadioButton) findViewById(R.id.mRadioButton2);
+        mRadioGroup = (RadioGroup) findViewById(R.id.mRadioGroup);
+
+        mRadioGroup.check(preferences.getInt("button",R.id.mRadioButton0));
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.mRadioButton0:
+                        editor.putInt("tune", R.raw.sound1);
+                        editor.putInt("button",R.id.mRadioButton0);
+                        editor.putString("text","鈴聲一");
+                        editor.apply();
+                        break;
+                    case R.id.mRadioButton1:
+                        editor.putInt("tune", R.raw.sound2);
+                        editor.putInt("button",R.id.mRadioButton1);
+                        editor.putString("text","鈴聲二");
+                        editor.apply();
+                        break;
+                    case R.id.mRadioButton2:
+                        editor.putInt("tune", R.raw.sound3);
+                        editor.putInt("button",R.id.mRadioButton2);
+                        editor.putString("text","鈴聲三");
+                        editor.apply();
+                        break;
+                }
+                sound.stopTune();
+                sound.chooseTrack(preferences.getInt("tune",0));
+                sound.playTune();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sound.stopTune();
+    }
+}
